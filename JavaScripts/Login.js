fd6b5ba07172557a8349dfec37e489b6f1155657
NicolasSP12 <nicolas_salas_1200@hotmail.com>
2022-02-16 22:42:45 -0300

    Cambios Login

--- JavaScripts/Login.js
+++ JavaScripts/Login.js
@@ -1,30 +1,46 @@
 var cuentas = [
-    {nombre= "Mali", saldo= 200},
-    {nombre= "Gera", saldo= 290},
-    {nombre="Maui", saldo= 67}
+  {nombre: "Mali", saldo: 200, password: 123},
+  {nombre: "Gera", saldo: 290, password: 789},
+  {nombre:"Maui", saldo: 67, password: 521}
 ]
 
-function login(){
-    if (form.usuario.value == "Maui") {
-      if(form.contraseña.value="123"){
+
+function login(form){ 
+    document.getElementById("usuario").innerHTML
+    for(var i = 0; i<cuentas.length; i++){
+      if(cuentas[i].nombre==="Mali" && cuentas[i].password==="123"){
         location="Main.html"
-      } else{
-        alert("No es la contraseña correcta")
-      }
-    } if (form.usuario.value == "Mali") {
-        if(form.contraseña.value="123"){
-          location="Main.html"
-      } else{
-          alert("No es la contraseña correcta")
-        }
-    } if (form.usuario.value == "Gera") {
-        if(form.contraseña.value="123"){
-          location="Main.html"
-      } else{
-          alert("No es la contraseña correcta")
+      }else if(cuentas[i].nombre==="Gera"&&cuentas[i].password==="789"){
+        location="Main.html"
+      } else if(cuentas[i].nombre==="Maui"&&cuentas[i].password==="521"){
+        location="Main.html"
+      }else{
+        alert("Usuarios y contraseñas no encontrados")
       }
     }
-  }
+}
+
+
+  //   if (form.usuario.value == cuentas["nombre"]) {
+  //     if(form.contraseña.value="123"){
+  //       location="Main.html"
+  //     } else{
+  //       alert("No es la contraseña correcta")
+  //     }
+  //   } if (form.usuario.value == "Mali") {
+  //       if(form.contraseña.value="123"){
+  //         location="Main.html"
+  //     } else{
+  //         alert("No es la contraseña correcta")
+  //       }
+  //   } if (form.usuario.value == "Gera") {
+  //       if(form.contraseña.value="123"){
+  //         location="Main.html"
+  //     } else{
+  //         alert("No es la contraseña correcta")
+  //     }
+  //   }
+  // }
 
 
 
--- JavaScripts/Scripts.js
+++ JavaScripts/Scripts.js
@@ -1,6 +1,2 @@
-var cuentas = [
-    {nombre= "Mali", saldo= 200},
-    {nombre= "Gera", saldo= 290},
-    {nombre="Maui", saldo= 67}
-]
+
     
