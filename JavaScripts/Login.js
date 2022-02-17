ac18b03f1d3ada1e0fa5d067203cacd6c8686c45
NicolasSP12 <nicolas_salas_1200@hotmail.com>
2022-02-17 18:01:56 -0300

    Agrega funciones de maximo y minimo para el cajero

--- JavaScripts/Login.js
+++ JavaScripts/Login.js
@@ -9,26 +9,25 @@ var usuario = ""
 function login(form){ 
   usuario = document.getElementById("usuario").value
   var contraseña = document.getElementById("contraseña").value
-    for(var i = 0; i<cuentas.length; i++){
-      if(cuentas[i].nombre==usuario && cuentas[i].password==contraseña){
-        location.href ="Main.html?usuario="+usuario
-      }else{
-        alert("Usuario no encontrado")
-        
+    for( var i = 0 ; i < cuentas.length ; i++ ){
+      if ( cuentas[i].nombre == usuario && cuentas[i].password == contraseña ){
+        location.href = "Main.html?usuario="+usuario
+      }else {
+        // alert("Usuario no encontrado")
+        // console.log("Usuario no encontrado")
       }
     } 
 }
 
 window.onload = function(){
-  mostrar(usuario)
   // const params = new URLSearchParams(window.location.search)
   // console.log(params)
-  
+  mostrar(usuario)
 }
 
 function mostrar(usuario){
   for( var i = 0; i < cuentas.length; i++ ){
-    if(cuentas[i].nombre==usuario){
+    if( cuentas[i].nombre == usuario ){
       document.getElementById("montos").value = cuentas[i].saldo
     }
   }
@@ -40,12 +39,27 @@ var limite_min = 10
 function formula1() {
   var CantidadTotal = parseInt(document.getElementById("cantidad").value)
   var CantidadInput = parseInt(document.getElementById("montos").value)
-  document.getElementById("montos").value=(CantidadTotal+CantidadInput)
-  
+  if(CantidadTotal < limite_max && CantidadInput < limite_max){
+    if((CantidadTotal+CantidadInput) <= 990){
+      document.getElementById("montos").value=(CantidadTotal+CantidadInput)
+    }else{
+      alert("No puede ingresar un monto mayor a " + 990)
+    }
+  }else{
+    alert("Ha alcanzado el limite maximo de la cuenta")
+  }
 }
+
 function formula2() {
   var CantidadTotal = parseInt(document.getElementById("cantidad").value)
   var CantidadInput = parseInt(document.getElementById("montos").value)
-  document.getElementById("montos").value=(CantidadInput-CantidadTotal)
-  
+  if(CantidadTotal >= limite_min){
+    if((CantidadInput-CantidadTotal) > 10){
+      document.getElementById("montos").value= (CantidadInput-CantidadTotal)
+    }else{
+      alert("No puede retirar más montos, su cuenta ha llegado al limite minimo: " + 10)
+    }
+  }else{
+    alert("Ha alcanzado el limite minimo de la cuenta")
+  }
 }
