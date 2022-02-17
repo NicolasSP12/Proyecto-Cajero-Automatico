cd044edef5f6034555d86b6ce5a545fa34923e6c
NicolasSP12 <nicolas_salas_1200@hotmail.com>
2022-02-17 01:23:27 -0300

    Agrega funciones de suma y resta, arregla algunos detalles en css y html

--- JavaScripts/Login.js
+++ JavaScripts/Login.js
@@ -4,18 +4,48 @@ var cuentas = [
   {nombre:"Maui", saldo: 67, password: 521}
 ]
 
+var usuario = ""
 
 function login(form){ 
-    document.getElementById("usuario").innerHTML
+  usuario = document.getElementById("usuario").value
+  var contraseña = document.getElementById("contraseña").value
     for(var i = 0; i<cuentas.length; i++){
-      if(cuentas[i].nombre==="Mali" && cuentas[i].password===123){
-        location="Main.html"
-      }else if(cuentas[i].nombre==="Gera"&&cuentas[i].password===789){
-        location="Main.html"
-      } else if(cuentas[i].nombre==="Maui"&&cuentas[i].password===521){
-        location="Main.html"
+      if(cuentas[i].nombre==usuario && cuentas[i].password==contraseña){
+        location.href ="Main.html?usuario="+usuario
       }else{
-        alert("Usuarios y contraseñas no encontrados")
+        alert("Usuario no encontrado")
+        
       }
+    } 
+}
+
+window.onload = function(){
+  mostrar(usuario)
+  // const params = new URLSearchParams(window.location.search)
+  // console.log(params)
+  
+}
+
+function mostrar(usuario){
+  for( var i = 0; i < cuentas.length; i++ ){
+    if(cuentas[i].nombre==usuario){
+      document.getElementById("montos").value = cuentas[i].saldo
     }
+  }
+}
+
+var limite_max = 990
+var limite_min = 10
+
+function formula1() {
+  var CantidadTotal = parseInt(document.getElementById("cantidad").value)
+  var CantidadInput = parseInt(document.getElementById("montos").value)
+  document.getElementById("montos").value=(CantidadTotal+CantidadInput)
+  
+}
+function formula2() {
+  var CantidadTotal = parseInt(document.getElementById("cantidad").value)
+  var CantidadInput = parseInt(document.getElementById("montos").value)
+  document.getElementById("montos").value=(CantidadInput-CantidadTotal)
+  
 }
